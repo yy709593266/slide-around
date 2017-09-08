24e3f80857e68f9a36034c7f1e875c7b9569402c
yy709593266 <709593266@qq.com>
2017-09-08 16:40:27 +0800

    come true

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# slide-around(暂未完成)
+# slide-around
 点击左右滑动实现左右无缝隙循环滑动
+
+## 实现原理
+左右滑动的原理图如下所示
+![原理图](http://ouampsjjo.bkt.clouddn.com/slide-around.jpg)
+其中,黑色框为窗口容器,用来显示当前展示的窗口,红色框是内容的容器,通过对内容容器添加相对定位属性并通过设置left值实现左右滑动效果:
+``
+.content-wrap {
+    position: relative;
+}
+``
+
--- js/script.js
+++ js/script.js
@@ -1,19 +1,56 @@
-(function(){
+function SlideAround(){
     //申请一下:
-    // 这里的方法都不考虑兼容性,揍似这么任性(因为我也不熟这些个兼容性,百度搜索吧...)
+    // 这里的方法都不考虑兼容性,揍似这么任性...
     var slideleft = document.getElementById('slideLeft');
     var slideRight = document.getElementById('slideRight');
     var contentWrap = document.getElementById('contentWrap');
+    // 左移动距离
+    var slideVal = -900;
+    // 克隆首尾元素
+    var firstContent = document.getElementsByClassName('left-content')[0];
+    var lastContent = document.getElementsByClassName('right-content')[0];
+    var cloneFirst = firstContent.cloneNode();
+    var cloneLast = lastContent.cloneNode();
+    contentWrap.insertBefore(cloneLast, firstContent);
+    contentWrap.appendChild(cloneFirst);
+    // 子元素个数
+    var itemNum = document.getElementsByClassName('content-item').length;
+    // 子元素宽度
+    var itemWidth = Number(window.getComputedStyle(document.getElementsByClassName('content-item')[0], null)['width'].split('px')[0]);
+    // window.getComputedStyle(contentWrap, null)['width'] = (itemWidth * itemNum) + "px";
+    contentWrap.style.width = itemNum * itemWidth + "px";
 
     //左滑动
     slideleft.addEventListener("click", function(){
-        
+        // var curLeft = Number(window.getComputedStyle(contentWrap, null)['left'].split('px')[0]);
+        slideVal += itemWidth;
+        if(slideVal > 0) {
+            contentWrap.style.transition = "none";
+            contentWrap.style.left = -(itemWidth * (itemNum - 2)) + "px";
+            slideVal = -(itemWidth * (itemNum - 3));
+        }
+        setTimeout(function(){
+            contentWrap.style.transition = "left 0.6s";
+            contentWrap.style.left = slideVal + 'px';
+        }, 20);
     });
 
     //右滑动
-    //ele.style.left只能获取内联样式的属性值,对于这种外联的样式没作用
+    //ele.style.left只能获取内联样式的属性值,对于外联的样式没作用
     slideRight.addEventListener("click", function(){
-        var left = Number(window.getComputedStyle(contentWrap, null)['left'].split('px')[0]);
-        console.log(left);
+        // var curLeft = Number(window.getComputedStyle(contentWrap, null)['left'].split('px')[0]);
+        slideVal -= itemWidth;
+        if(slideVal < -(itemWidth * (itemNum - 1))) {
+            contentWrap.style.transition = "none";
+            contentWrap.style.left = -itemWidth + "px";
+            slideVal = -(itemWidth * 2);
+        }
+        // 需要等到将位置移到第二个元素之后再重新开启transition属性,不设置setTimeout的话后面的transition动画属性会直接覆盖了前面取消动画的操作
+        setTimeout(function(){
+            contentWrap.style.transition = "left 0.6s";
+            contentWrap.style.left = slideVal + 'px';
+        }, 20);
     });
-})()
+}
+
+new SlideAround();
